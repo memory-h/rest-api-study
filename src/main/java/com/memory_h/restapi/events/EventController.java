77cabb7ce05c66043e9ac002c2430aafae8d2bb5
Hyeon Young Jang <gusdud9124@gmail.com>
2024-10-01 01:05:10 +0900

    이벤트 테스트 201 응답 받기

---
+++ src/main/java/com/memory_h/restapi/events/EventController.java
@@ -0,0 +1,34 @@
+package com.memory_h.restapi.events;
+
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+
+@RestController
+@RequestMapping(value = "/api/events", produces = MediaTypes.HAL_JSON_VALUE) // 해당 타입으로 응답을 보낸다.
+public class EventController {
+
+    /**
+     * linkTo(EventController.class): EventController 클래스의 경로(URI)를 기준으로 링크를 생성하는 역할을 한다.
+     * EventController 클래스가 @RequestMapping 어노테이션을 통해 매핑된 기본 URL 경로를 기반으로 한다.
+     * 즉, 기본 경로는 /api/events 이다.
+     *
+     * slash("{id}")는 기본 경로에 추가적인 세그먼트를 붙이는 작업을 한다. 예: /api/events/10
+     * toUri(): 실제 URI 객체로 변환한다.
+     */
+    @PostMapping
+    public ResponseEntity<Event> createEvent(@RequestBody Event event) {
+        URI createUri = linkTo(EventController.class).slash("{id}").toUri();
+        event.setId(10);
+
+        return ResponseEntity.created(createUri).body(event);
+    }
+
+}
--- src/test/java/com/memory_h/restapi/events/EventControllerTest.java
+++ src/test/java/com/memory_h/restapi/events/EventControllerTest.java
@@ -1,5 +1,6 @@
 package com.memory_h.restapi.events;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -7,7 +8,11 @@ import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
@@ -16,13 +21,32 @@ public class EventControllerTest {
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
     @Test
     void createEvent() throws Exception {
+        Event event = Event.builder()
+                .name("Spring")
+                .description("REST API Development with Spring")
+                .beginEnrollmentDateTime(LocalDateTime.of(2024, 9, 30, 10, 37))
+                .closeEnrollmentDateTime(LocalDateTime.of(2024, 10, 1, 10, 37))
+                .beginEventDateTime(LocalDateTime.of(2024, 10, 2, 10, 37))
+                .endEventDateTime(LocalDateTime.of(2024, 10, 3, 10, 37))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남역 D2 스타텁 팩토리")
+                .build();
+
         mockMvc.perform(post("/api/events")
-                        .contentType(MediaType.APPLICATION_JSON_UTF8)
-                        .accept(MediaTypes.HAL_JSON)
+                        .contentType(MediaType.APPLICATION_JSON) // 요청 또는 응답에서 전송되는 데이터의 타입, 즉 내가 보내는 데이터의 형식
+                        .accept(MediaTypes.HAL_JSON_VALUE) // 클라이언트가 서버에게 어떤 형식의 응답 데이터를 받을 수 있는지, 즉 내가 받고 싶은 데이터의 형식
+                        .content(objectMapper.writeValueAsString(event)) // json으로 바꾸고 본문에 넣어준다.
                 )
-                .andExpect(status().isCreated());
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("id").exists()); // id가 있는지
     }
 
 }
