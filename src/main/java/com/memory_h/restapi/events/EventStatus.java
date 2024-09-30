28a42a415648245d4df95cf862aabd98a4942d66
Hyeon Young Jang <gusdud9124@gmail.com>
2024-09-30 21:49:41 +0900

    Event domain

---
+++ src/main/java/com/memory_h/restapi/events/EventStatus.java
@@ -0,0 +1,7 @@
+package com.memory_h.restapi.events;
+
+public enum EventStatus {
+
+    DRAFT, PUBLISHED, BEGAN_ENROLLMENT
+
+}
---
+++ src/test/java/com/memory_h/restapi/events/EventTest.java
@@ -0,0 +1,39 @@
+package com.memory_h.restapi.events;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class EventTest {
+
+    @Test
+    void builder() {
+        Event event = Event.builder()
+                .name("Inflearn Spring REST API")
+                .description("REST API development with Spring")
+                .build();
+
+        assertThat(event).isNotNull();
+    }
+
+    @Test
+    void javaBean() {
+        // given
+        String name = "Event";
+        String description = "Spring";
+
+        // when
+        Event event = new Event();
+        event.setName(name);
+        event.setDescription(description);
+
+        // then
+        assertThat(event.getName()).isEqualTo("Event");
+        assertThat(event.getDescription()).isEqualTo("Spring");
+    }
+
+}
